fa2ebfba37a066743c621da9e75a74c7d0cf6c7e
Jesse <jesse.a.lapointe@gmail.com>
2021-06-21 20:10:42 +0000

    Create employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,44 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(
+  first_name: "Khurram", 
+  last_name: "Virani", 
+  hourly_rate: 60
+)
+@store2.employees.create(
+  first_name: "Jesse",
+  last_name: "LaPointe",
+  hourly_rate: 200
+)
+@store2.employees.create(
+  first_name: "Benny",
+  last_name: "Harvey",
+  hourly_rate: 40
+)
+@store1.employees.create(
+  first_name: "Brian",
+  last_name: "Limmond",
+  hourly_rate: 70
+)
+@store2.employees.create(
+  first_name: "Tin",
+  last_name: "Tin",
+  hourly_rate: 40
+)
+@store1.employees.create(
+  first_name: "Norm",
+  last_name: "Macdonald",
+  hourly_rate: 60
+)
+@store1.employees.create(
+  first_name: "Karl Ove",
+  last_name: "Knausgaard",
+  hourly_rate: 40
+)
+@store2.employees.create(
+  first_name: "Chetna",
+  last_name: "Dickerson",
+  hourly_rate: 80
+)
